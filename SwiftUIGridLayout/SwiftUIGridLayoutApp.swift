24aac0bf5db51b52d183f40027db092397c21ba4
Ajay Girolkar <ajaygirolkar@gmail.com>
2021-11-22 10:42:47 +0530

    Initial commit

---
+++ SwiftUIGridLayout/SwiftUIGridLayoutApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIGridLayoutApp.swift
+//  SwiftUIGridLayout
+//
+//  Created by Ajay Girolkar on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridLayoutApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
